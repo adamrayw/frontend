34b56e2a3fba123b5fb5b9e113c31635dbeb8820
adamrayw <raywibowo68@gmail.com>
2023-07-11 00:53:30 +0700

    add logout button

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -9,11 +9,19 @@ import Others from '../assets/others.png'
 import Watch from '../assets/icon-watch.png'
 import background from '../assets/backgroundOpacity.png'
 import Logo from '../assets/Logo.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Dashboard() {
 
     const user = JSON.parse(localStorage.getItem('user'))
+
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem('user')
+        localStorage.removeItem('credential')
+        navigate('/login')
+    }
 
     return (
         <div className="relative dashboard mx-auto h-screen max-w-sm px-4 pb-10 pt-4 "
@@ -24,9 +32,25 @@ function Dashboard() {
                 backgroundPosition: 'bottom',
             }}
         >
-            <div className="dashboard-username flex justify-between items-center">
-                <h1 className="text-lg font-bold">Selamat Sore! {user.name}</h1>
-                <img src={Logo} className="logo h-10 w-10 rounded-full" />
+            <div className="dashboard-username">
+                <div className='flex justify-between items-center'>
+                    <h1 className="text-lg font-bold">Selamat Sore! {user.name}</h1>
+                    <img src={Logo} className="logo h-10 w-10 rounded-full" />
+                </div>
+                <div className='logout'>
+                    <button onClick={handleLogout} className='flex text-red-400 font-semibold bg-red-200 py-1 px-2 rounded-lg items-center'>
+                        <svg
+                            viewBox="0 0 900 1000"
+                            fill="currentColor"
+                            height="1em"
+                            width="1em"
+                            className='mr-1'
+                        >
+                            <path d="M502 850V750h98v100c0 26.667-9.667 50-29 70s-43 30-71 30H100c-26.667 0-50-10-70-30S0 876.667 0 850V150c0-28 10-51.667 30-71s43.333-29 70-29h400c28 0 51.667 9.667 71 29s29 43 29 71v150h-98V150H100v700h402m398-326L702 720V600H252V450h450V330l198 194" />
+                        </svg>
+                        Keluar
+                    </button>
+                </div>
             </div>
             <div className="dashboard-menu grid grid-cols-4 gap-y-6 mt-10">
                 <div>
